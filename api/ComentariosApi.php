870eefc5284f2e0e6581798bea32f83bdfee7903
Lucas González <lucas.acdc92@gmail.com>
2016-12-01 03:21:32 -0300

    Api terminada

--- api/ComentariosApi.php
+++ api/ComentariosApi.php
@@ -1,63 +1,51 @@
 <?php
-require_once 'api.php';
-require_once ('../models/ComentariosModel.php');
-include_once(dirname(__DIR__)."/controllers/UsuariosController.php"); //revisar
-class ComentariosApi extends Api {
-  private $modelo;
-  private $usuariosController;
+require 'api.php';
+include_once '../models/ComentariosModel.php';
+include_once(dirname(__DIR__)."/controllers/LoginController.php");
+
+
+class ComentariosApi extends Api
+{
+  private $model;
   public function __construct($request)
-  {
+ {
     parent::__construct($request);
-    $this->modelo = new ComentariosModel();
-    $this->usuariosController = new UsuariosController();
+    $this->model = new ComentariosModel();
   }
-  protected function comentario($argumentos){
+  protected function comentarios($argumentos){
     switch ($this->method) {
       case 'GET':
-      if(count($argumentos)>0){
-        $comentario = $this->modelo->getComentariosProducto($argumentos[0]);
-        $error['Error'] = "No exiten comentarios";
-        return ($comentario) ? $comentario : $error;
-      }else{
-        return $this->modelo->getComentarios();
-      }
-      break;
-      case 'DELETE':
-      if ($this->usuariosController->getRol()==1) { //cambiar
         if(count($argumentos)>0){
+          $Comentario = $this->model->getComentario($argumentos[0]);
           $error['Error'] = "El comentario no existe";
-          $success['Success'] = "El comentario se ha borrado";
-          $filasAfectadas = $this->modelo->eliminarComentario($argumentos[0]);
-          return ($filasAfectadas == 1) ? $success : $error;
-        }
-      }
-      $error['Error'] = "Se ha denegado el acceso";
-      return $error;
-      break;
-      case 'POST':
-      if ($this->usuariosController->checkLogin()) { //cambiar
-        if(count($argumentos)==0){
-          if(isset($_POST['id_paquete']) && isset($_POST['email']) && isset($_POST['comentario'])) {
-            $id_paquete = $_POST['id_paquete'];
-            $usuario = $_POST['email'];
-            $comentario = $_POST['comentario'];
-            if(isset($_POST['rating'])) {
-              $rating = $_POST['rating'];
-            }
-            else $rating = 0;
-            $error['Error'] = "El comentario no se creo";
-            $id_comentario = $this->modelo->crearComentario($id_paquete,$usuario,$comentario,$rating);
-            return ($id_comentario > 0) ? $this->modelo->getComentario($id_comentario) : $error;
+            return ($Comentario) ? $Comentario : $error;
+          }else{
+            return $this->model->getComentarios();
           }
-        }
-      }
-      $error['Error'] = "Se ha denegado el acceso";
-      return $error;
-      break;
+          break;
+        case 'DELETE':
+         if(count($argumentos)>0){
+            $error['Error'] = "El comentario no existe";
+           $success['Success'] = "El comentario se borro";
+            $filasAfectadas = $this->model->eliminarComentario($argumentos[0]);
+           return ($filasAfectadas == 1) ? $success : $error;
+          }
+       break;
+        case 'POST':
+            if(count($argumentos)==0){
+              $error['Error'] = "El comentario no se creo";
+              $comentario = $_POST['comentario'];
+              $id_producto = $_POST['id_producto'];
+              $puntaje = $_POST['puntaje'];
+              $id_comentario = $this->model->crearComentario($comentario,$puntaje,$id_producto );
+              $error['Error'] = "asd";
+              return ($id_comentario > 0) ? $this->model->getComentario($id_comentario) : $error;
+            }
+          break;
       default:
-      return "Only accepts GET";
-      break;
+           return "Only accepts GET";
+        break;
     }
-  }
+   }
 }
-?>
+ ?>
