29a528278fda0160266e3124bd1bb07969cea091
María José Bandi <majo.bandi@gmail.com>
2020-09-30 20:00:27 -0300

    eliminacion de comentario en controllers/NtdController.php

--- controllers/NtdController.php
+++ controllers/NtdController.php
@@ -6,7 +6,7 @@ include_once 'models/NtdModel.php';
 class NtdController   {
 // Función que muestra la nueva sección de la página
 // Función que muestra por items la nuueva sección
-// Función de test
+
 
   function __construct() {
     $this->modelo = new NtdModel();
